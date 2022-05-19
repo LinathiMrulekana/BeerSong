354aba40f1e899d69edc945fec96d67e2dae9ba6
LinathiMrulekana <102727009+LinathiMrulekana@users.noreply.github.com>
2022-05-19 09:39:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment_8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song info = new Song(99);
+            info.SingSong();
+        }
+    }
+}
---
+++ Song.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8_1
+{
+    internal class Song
+    {
+        private int _icount;
+        private string _lyrics1;
+        private string _lyrics2;
+        private string _chorus;
+
+        public Song(int icount)
+        {
+            this._icount = icount;
+            this._lyrics1 = "bottles of beer on the wall,";
+            this._lyrics2 = "bottles of beer.";
+            this._chorus = "Take one down and pass it around";
+
+        }
+
+        public void SingSong()
+        {
+            for (int i = _icount; i > 0; i--)
+            {
+                Console.WriteLine(i + " " + _lyrics1 + i + " " + _lyrics2);
+                Console.WriteLine(_chorus + " " + (i - 1) + " " + _lyrics1);
+                if (i < 1)
+                {
+                    Console.WriteLine(i + "  " + "bottle of beer on the wall,"+ i +" "+ "bottle of beer.");
+                    Console.WriteLine(_chorus + " " + (i - 1) + " " + "bottle of beer on the wall,");
+                }
+            }
+        }
+    }
+}
+
